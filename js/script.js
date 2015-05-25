35c5b6337e769a4a246e76a7baa1219a211f2547
sukhchand <sukhchand.k@echidnainc.com>
2015-05-25 18:33:19 +0530

    updates

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+    $(".popular_searches").outerHeight($(".search_popup").height());
+    $(".search_field").on("click", function(){
+        $(".search_popup").slideToggle();
+    })
+});
