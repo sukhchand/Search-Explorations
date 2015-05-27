5bc04ffd9f747f068c38ceac3deb0d1398427423
sai-echidnainc <saicharan.c@echidnainc.com>
2015-05-27 12:59:09 +0530

    Updated Changes

--- js/script.js
+++ js/script.js
@@ -23,3 +23,27 @@ app.service('searchService',function($http){
     	.success(success);
 	};	
 });
+
+$( document ).ready(function() {
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+    $(".popular_searches").outerHeight($(".search_popup").height());
+    $(".search_field").on("click", function(){
+        $(".search_popup").slideToggle();
+    });
+    $('.popular_searches li a').on('click', function(){
+        $('.products').css('display','none');
+        $('.categories').css('display','none');
+        $('.products_selected').css('display','block');
+
+    })
+});
